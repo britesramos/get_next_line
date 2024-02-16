934b919e2e48a5a9ceaf3a06dd12c9cc5bf9c82f
britesramos <148062118+britesramos@users.noreply.github.com>
2024-02-16 14:46:26 +0100

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sramos <sramos@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/02/06 16:08:12 by sramos        #+#    #+#                 */
+/*   Updated: 2024/02/16 12:23:34 by sramos        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_read(int fd, char *buffer);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_leftovers(char *buffer, char *line);
+char	*ft_extline(char *buffer);
+void	ft_free(char **ptr);
+
+#endif
